2caec957435c5c7bf1010abbbd6b2e75ad26daa5
Varad Gautam <varad.gautam@suse.com>
2021-03-08 16:49:07 +0100

    keyctl_pkey: Add pkey parameters slen and mgfhash for PSS
    keyctl pkey_* operations accept enc and hash parameters at present.
    RSASSA-PSS signatures also require passing in the signature salt
    length and the mgf hash function.
    
    Add parameters:
    - 'slen' to feed in salt length of a PSS signature.
    - 'mgfhash' to feed in the hash function used for MGF.
    
    Signed-off-by: Varad Gautam <varad.gautam@suse.com>

--- security/keys/keyctl_pkey.c
+++ security/keys/keyctl_pkey.c
@@ -24,11 +24,15 @@ enum {
 	Opt_err,
 	Opt_enc,		/* "enc=<encoding>" eg. "enc=oaep" */
 	Opt_hash,		/* "hash=<digest-name>" eg. "hash=sha1" */
+	Opt_slen,		/* "slen=<salt-length>" eg. "slen=32" */
+	Opt_mgfhash,		/* "mgfhash=<digest-name>" eg. "mgfhash=sha1" */
 };
 
 static const match_table_t param_keys = {
 	{ Opt_enc,	"enc=%s" },
 	{ Opt_hash,	"hash=%s" },
+	{ Opt_slen,	"slen=%u" },
+	{ Opt_mgfhash,	"mgfhash=%s" },
 	{ Opt_err,	NULL }
 };
 
@@ -61,6 +65,15 @@ static int keyctl_pkey_params_parse(struct kernel_pkey_params *params)
 
 		case Opt_hash:
 			params->hash_algo = q;
+			break;
+
+		case Opt_slen:
+			if (kstrtouint(q, 0, &params->slen))
+				return -EINVAL;
+			break;
+
+		case Opt_mgfhash:
+			params->mgf_hash_algo = q;
 			break;
 
 		default:
